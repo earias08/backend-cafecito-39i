255019ca018870b4bd2bb21eb4b0ffad5a104b3e
JonyPlo <joonyyplo@gmail.com>
2023-06-26 11:33:37 -0300

    Se creo la ruta y controlador para poder crear un objeto en la base de datos

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,11 +1,30 @@
 import Producto from '../models/producto';
 
+// Controlador para obtener productos
 export const obtenerProductos = async (req, res) => {
   try {
     const productos = await Producto.find();
     res.status(200).json(productos);
   } catch (error) {
     console.log(error);
+    res.status(404).json({
+      mensaje: 'Error al buscar los productos',
+    });
   }
 };
 
+// Controlador para crear un producto
+export const crearProducto = async (req, res) => {
+  try {
+    const productoNuevo = new Producto(req.body);
+    await productoNuevo.save();
+    res.status(201).json({
+      mensaje: 'El producto fue creado correctamente',
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      mensaje: 'Error al crear al producto',
+    });
+  }
+};
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,12 +1,17 @@
-import { Router } from "express";
-import { obtenerProductos } from "../controllers/productos.controllers";
+import { Router } from 'express';
+import {
+  crearProducto,
+  obtenerProductos,
+} from '../controllers/productos.controllers';
 
-const router = Router()
+const router = Router();
 
 // app.get('/prueba', (req, res)=>{
 //   res.send('esto es un prueba de la peticion GET a mi backend');
 // })
 
-router.route('/productos').get(obtenerProductos)
+router.route('/productos')
+.get(obtenerProductos)
+.post(crearProducto);
 
-  export default router
+export default router;
