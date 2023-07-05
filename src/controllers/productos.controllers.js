5460a11a4cbbd82b28df6a657f0239ac9a81aa65
Emilse Arias <emilsearias@gmail.com>
2023-07-05 10:09:27 -0300

    agregamos validaciones en el nombre del producto y el precio

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -20,6 +20,7 @@ export const crearProducto = async (req, res) => {
     //trabajar con el resultado de la validacion
     const errors = validationResult(req);
     // errors.isEmpty() true: si esta vacio
+    //pregunto si hay errores
     if(!errors.isEmpty()){
       return res.status(400).json({errores: errors.array()})
     }
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -21,7 +21,21 @@ router
     [
       check("nombreProducto")
         .notEmpty()
-        .withMessage("El nombre del producto es un dato obligatori"),
+        .withMessage("El nombre del producto es un dato obligatorio")
+        .isLength({min: 2, max:100})
+        .withMessage('El nombre del producto debe contener entre 2 y 100 caracteres inclusive'),
+      check('precio')
+        .notEmpty()
+        .withMessage('El precio es un dato obligatorio')
+        .isNumeric()
+        .withMessage('El precio debe ser un número')
+        .custom((value)=>{
+          if(value >= 1 && value <= 10000){
+            return true;
+          }else{
+            throw new Error('El precio debe estar entre 1 y 10000')
+          }
+        })
     ],
     crearProducto
   );
